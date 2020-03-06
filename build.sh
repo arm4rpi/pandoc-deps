dbba23b93cb9f2cdbb8bac35877257304b4e19ee
annprog <tecbbs@qq.com>
2020-03-07 04:26:13 +0800

    verbose

--- build.sh
+++ build.sh
@@ -10,13 +10,13 @@ apt-get install -y cabal-install
 
 echo "I am `whoami`, I will build $ARCH-$PKG"
 echo "cabal init"
-cabal user-config init
+cabal user-config init -v
 
 echo "v2-update"
-cabal v2-update
+cabal v2-update -v
 
 echo "v2-install $PKG"
-cabal v2-install $PKG
+cabal v2-install $PKG -v
 
 
 tar zcvf $ARCH-$PKG.tar.gz /root/.cabal/store/ghc-8.6.5/
