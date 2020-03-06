0deeb42144a652c1a19d3ba1ecd7b8a33204ea76
annprog <tecbbs@qq.com>
2020-03-07 03:49:01 +0800

    test

--- build.sh
+++ build.sh
@@ -6,12 +6,23 @@ set -e
 
 ARCH=`arch`
 
+echo "I am `whoami`, I will build $ARCH-$1"
+
+cd root
+
 apt-get update
 apt-get install -y cabal-install
 
+echo "cabal init"
 cabal user-config init
+
+echo "v2-update"
 cabal v2-update
+
+echo "v2-install $1"
 cabal v2-install $1
 
 
 tar zcvf $ARCH-$1.tar.gz /root/.cabal/store/ghc-8.6.5/
+
+echo "Finish $ARCH-$1.tar.gz"
