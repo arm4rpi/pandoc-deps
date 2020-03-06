279e010f7b8708c6589cbe7803d1abb7c6d95063
annprog <tecbbs@qq.com>
2020-03-07 04:42:29 +0800

    new-install

--- build.sh
+++ build.sh
@@ -15,8 +15,8 @@ cabal user-config init
 echo "v2-update"
 cabal v2-update
 
-echo "v2-build $PKG"
-cabal v2-build $PKG
+echo "new-install $PKG"
+cabal new-install -v $PKG
 
 
 tar zcvf $ARCH-$PKG.tar.gz /home/runner/.cabal/store/ghc-8.6.5/
