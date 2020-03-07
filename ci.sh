9c6a98ddded65952467bb2f458f9c7d0306daa39
annprog <tecbbs@qq.com>
2020-03-07 20:20:47 +0800

    fix bugs

--- ci.sh
+++ ci.sh
@@ -21,10 +21,10 @@ for id in `cat deps.txt |sed '/^$/d'|grep -v "#" |sort -u`;do
     - uses: actions/checkout@v2
     - name: build
       run: |
-        curl -s -L "https://github.com/arm4rpi/pandoc-deps/releases/download/v0.1/aarch64-$id.tar.gz" -o aarch64-$id.tar.gz
-        MIME=\`file -b --mime-type aarch64-$id.tar.gz\`
+        curl -s -L "https://github.com/arm4rpi/pandoc-deps/releases/download/v0.1/aarch64-$id.tar.gz" -o rootfs/aarch64-$id.tar.gz
+        MIME=\`file -b --mime-type rootfs/aarch64-$id.tar.gz\`
         echo \$MIME
-        [ "\$MIME"x == "application/x-gzip"x ] && echo "Already exists" && exit 0 || echo "Not exists"
+        [ "\$MIME"x == "application/gzip"x ] && echo "Already exists" && exit 0 || echo "Not exists"
         sudo apt-get update
         sudo apt-get install -y qemu-user-static aria2
         aria2c -x 16 http://cdimage.ubuntu.com/ubuntu-base/releases/19.10/release/ubuntu-base-19.10-base-arm64.tar.gz
@@ -58,8 +58,8 @@ for id in `cat deps.txt |sed '/^$/d'|grep -v "#" |sort -u`;do
     - uses: actions/checkout@v2
     - name: build
       run: |
-        curl -s -L "https://github.com/arm4rpi/pandoc-deps/releases/download/v0.1/armv7l-$id.tar.gz" -o armv7l-$id.tar.gz
-        MIME=\`file -b --mime-type armv7l-$id.tar.gz\`
+        curl -s -L "https://github.com/arm4rpi/pandoc-deps/releases/download/v0.1/armv7l-$id.tar.gz" -o rootfs/armv7l-$id.tar.gz
+        MIME=\`file -b --mime-type rootfs/armv7l-$id.tar.gz\`
         echo \$MIME
         [ "\$MIME"x == "application/gzip"x ] && echo "Already exists" && exit 0 || echo "Not exists"
         sudo apt-get update
