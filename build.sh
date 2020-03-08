b0237844d4dfa6475a2fed6f36c8073438cc469c
annprog <tecbbs@qq.com>
2020-03-09 03:54:24 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ cabal v2-update
 
 echo "v2-install $PKG"
 mkdir -p /home/runner/.cabal/store/ghc-8.6.5/package.db
-cabal v2-install -v $PKG
+cabal v2-install $PKG
 
 
 tar zcvf $ARCH-$PKG.tar.gz /home/runner/.cabal/store/ghc-8.6.5/
